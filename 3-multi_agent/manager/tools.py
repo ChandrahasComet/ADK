c51947e8f3117e228974654b42fe14645c4f577a
Chandrahas <chandrahasreddy1606@gmail.com>
2025-06-18 11:35:26 +0530

    feat: Refactor tools and update .gitignore

--- 3-multi_agent/manager/tools.py
+++ 3-multi_agent/manager/tools.py
@@ -1,13 +1,6 @@
 import datetime
 import yfinance as yf
 
-def get_current_time(format: str)->dict:
-    """
-    Get the current time in the mentioned format.
-    """
-    return {
-        "current_time": datetime.datetime.now().strftime(format) if format else datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    }
 
 def get_stock_price(company_name: str) -> dict:
     """
